18664b52336c51c48cc8b935d2a44d856f698e16
piyush Kumar <piyush25032@gmail.com>
2019-02-25 20:15:16 +0530

    swap Pairwise Node

---
+++ Linked-List/Single-Linked-List/12_swapPairwiseNode.cpp
@@ -0,0 +1,194 @@
+/**
+ *  author:- piyushkumar96
+ *  description:- swapping pairwise nodes 1->2->3->4->5 to 2->1->4->3->5 (Recursive and Iterative)
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int item;
+    struct Node* next;
+    Node(int item){
+        this->item=item;
+        next=NULL;
+    }
+} *HEAD;
+
+struct LinkedList {
+    
+    LinkedList(){
+        HEAD=NULL;
+    }
+    void insert(int item){
+        if(HEAD == NULL){
+            Node* newnode = new Node(item);
+            HEAD=newnode;
+            cout<<"Successfully Inserted "<<"\n";
+        }else {
+                Node* CURR = HEAD;
+                while(CURR->next != NULL){
+                    CURR=CURR->next;
+                }
+                Node* newnode = new Node(item);
+                CURR->next=newnode;
+                cout<<"Successfully Inserted "<<"\n";
+        }
+    }
+
+    void swapData(struct Node **a, struct Node **b){
+        int temp = (*a)->item;
+        (*a)->item = (*b)->item;
+        (*b)->item = temp;
+    }
+
+    void swapLink(struct Node *a, struct Node *b){
+        cout<<"a "<<a->item<<" b "<<b->item<<"\n";
+        struct Node *temp = a;
+        (a)->next = (b)->next;
+        (b)->next = a;
+        cout<<"a "<<a->item<<" b "<<b->item<<"\n";
+        //return a;
+    }
+
+
+    // recursive method by swaping datas of Nodes
+    void recursivePairwiseSwap(struct Node *head_ref){
+
+        if(HEAD == NULL){
+            cout<<"Linked List is empty "<<"\n";
+        }else{
+            if((head_ref == NULL) || (head_ref->next == NULL) ){
+                return;                   
+            }
+            swapData(&head_ref, &(head_ref->next));
+            recursivePairwiseSwap(head_ref->next->next);
+        }
+    }
+
+    // recursive method by swaping linkes
+    void recursivePairwiseSwapLink(struct Node *head_ref){
+        if(HEAD == NULL){
+            cout<<"Linked List is empty "<<"\n";
+        }else{
+            if((head_ref == NULL) || (head_ref->next == NULL) ){
+                return;                   
+            }
+            HEAD = HEAD->next;
+            swapLink(head_ref, (head_ref->next));
+            recursivePairwiseSwap(head_ref->next);
+        }
+    }
+    
+    // iterative method by swaping datas of Nodes
+    void iterativePairwiseSwap(){
+        if(HEAD == NULL){
+            cout<<"Linked List is empty "<<"\n";
+        }else{
+            struct Node *temp = HEAD;
+            while(temp != NULL && temp->next != NULL){
+                swapData(&temp , &(temp->next));
+                temp = temp->next->next;            
+            }
+        }
+    }
+
+        // iterative method by swaping links
+    void iterativePairwiseSwapLink(){
+        if(HEAD == NULL){
+            cout<<"Linked List is empty "<<"\n";
+        }else{
+            struct Node *temp = HEAD;
+            struct Node *temp1 = NULL;
+            HEAD = HEAD->next;
+            
+            while(temp != NULL && temp->next != NULL){
+                temp1 = temp->next->next;
+                cout<<"temp1 "<<temp1->item;
+                swapLink(temp , (temp->next));
+                temp = temp1;          
+            }
+        }
+    }
+
+    void deleteLinkedList(){
+        if(HEAD == NULL){
+            cout<<"Linked list is empty "<<"\n";
+        }else{
+            Node* CURR = HEAD;
+            Node* NEXT ;
+            while(CURR != NULL){
+                NEXT = CURR->next;
+                delete(CURR);
+                CURR = NEXT;
+            }
+            HEAD = NULL;
+            cout<<"Linked List is Successfully deleted"<<"\n";
+        }
+    }
+
+    void printLL(){
+        Node* CURR = HEAD;
+        if(CURR == NULL){
+            cout<<" Linked List is empty. Please insert element first"<<"\n";
+        }else {
+            cout<<"Linked List:- ";
+            while(CURR != NULL){
+                cout<<CURR->item<<" ";
+                CURR=CURR->next;
+            }
+            cout<<"\n";
+        }
+    }
+
+};
+
+int main(){
+   //ios::sync_with_stdio(false);
+   //cin.tie(0);
+   int i, item;
+   LinkedList ll;
+   while(1){
+       cout<<"\nSimple Linked List \n 1. insert \n 2. iterative Pairwise Swap \n 3. recursive Pairwise Swap \n 4. iterative Pairwise Link Swap \n 5. recursive Pairwise Link Swap  \n 6. delete Linked List \n 7. Display \n Press any key to Exit \n";
+       cin>>i;
+       switch (i)
+       {
+           case 1:  cout<<" Enter element to insert (integer only)";
+                    cin>>item;
+                    ll.insert(item);
+                    break;
+
+           case 2:  ll.iterativePairwiseSwap();
+                    cout<<"After pairwise swapping the  ";
+                    ll.printLL();
+                    break;
+
+           case 3:  ll.recursivePairwiseSwap(HEAD);
+                    cout<<"After pairwise swapping the  ";
+                    ll.printLL();
+                    break;
+
+           case 4:  ll.iterativePairwiseSwapLink();
+                    cout<<"After pairwise swapping the  ";
+                    ll.printLL();
+                    break;                    
+
+           case 5:  ll.recursivePairwiseSwapLink(HEAD);
+                    cout<<"After pairwise swapping the  ";
+                    ll.printLL();
+                    break;
+
+           case 6:  ll.deleteLinkedList();
+                    break;  
+
+           case 7:  ll.printLL();
+                    break;     
+
+           default: exit(1);
+                    break;
+       }
+
+   }
+
+    return 0;
+}
