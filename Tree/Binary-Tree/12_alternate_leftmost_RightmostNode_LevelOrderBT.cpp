8ccfeca6477e7e84c07e743ecf2c12ee412d090c
Piyush Kumar <piyush25032@gmail.com>
2019-09-15 18:40:24 +0530

    alternate leftmost rightmost node level order

--- Tree/Binary-Tree/12_alternate_leftmost_RightmostNode_LevelOrderBT.cpp
+++ Tree/Binary-Tree/12_alternate_leftmost_RightmostNode_LevelOrderBT.cpp
@@ -1,6 +1,7 @@
 /**
  *  author:- piyushkumar96
  *  description:- alternate leftmost and rightmost node in levelorder of Binary Tree
+ *  Time Complexity:- O(n) time
 **/
 
 #include<bits/stdc++.h>
@@ -71,6 +72,10 @@ void BinaryTree::alternateLeftmostRighmostLevelOrder(struct Node *root){
 // level order traversal iterative function
 void BinaryTree::levelorderTraversal(struct Node *root){
    
+   if(root == NULL){
+       cout<<"Tree is Empty \n";
+       return ;
+   }
    queue<Node *> qu;
    qu.push(root);
    while(!qu.empty()){
