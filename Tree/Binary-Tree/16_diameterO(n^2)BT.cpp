9331a975a14165b6e9d2289e54e62ab88ce511f4
Piyush Kumar <piyush25032@gmail.com>
2019-09-15 18:43:32 +0530

    diameter of BT in O(n^2)

--- Tree/Binary-Tree/16_diameterO(n^2)BT.cpp
+++ Tree/Binary-Tree/16_diameterO(n^2)BT.cpp
@@ -73,6 +73,10 @@ int BinaryTree::diameter(struct Node *root){
 // level order traversal iterative function
 void BinaryTree::levelorderTraversal(struct Node *root){
    
+   if(root == NULL){
+       cout<<"Tree is Empty \n";
+       return ;
+   }
    queue<Node *> qu;
    qu.push(root);
    while(!qu.empty()){
