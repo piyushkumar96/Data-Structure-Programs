b77b6974b1c2f8872aeb6bd69e1d561cd850d711
Piyush Kumar <piyush25032@gmail.com>
2019-09-15 18:46:56 +0530

    connect Node with same level

--- Tree/Binary-Tree/21_connectNode_SameLevelBT.cpp
+++ Tree/Binary-Tree/21_connectNode_SameLevelBT.cpp
@@ -69,6 +69,10 @@ void BinaryTree::connectNodes(struct Node *root)
 // level order traversal iterative function
 void BinaryTree::levelorderTraversal(struct Node *root){
    
+   if(root == NULL){
+       cout<<"Tree is Empty \n";
+       return ;
+   }
    queue<Node *> qu;
    qu.push(root);
    while(!qu.empty()){
